9788ed1831f275e444f56ba7b1b3ec09ba7716b5
zhangsan <zhangsan@163.com>
2018-11-12 22:58:17 +0800

    delete product.java

--- product.java
+++
@@ -1,8 +0,0 @@
-public class Product {
-	public static void main(String[] args) {
-		System.out.println("Hello Git!");
-		System.out.println("One");
-		System.out.println("Two");
-		System.out.println("Three");
-	}	
-}
